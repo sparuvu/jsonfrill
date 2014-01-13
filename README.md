e49e994630a7a8536babcd855f8ec6c9d9f4a97e
Sundeep Paruvu <sparuvu@gmail.com>
2014-01-13 10:13:09 -0600

    Minor Improvements: Added collapse-option + perf-improvements + icons
    commit b2047a51a7ec4c2a51217e61d46d8400b35c75f1
    Author: Sundeep Paruvu <sparuvu@gmail.com>
    Date:   Mon Jan 13 10:12:01 2014 -0600
    
        Added the expand and collapse icons
    
    commit ab8bb7c53ddb847f163d990554536b38d005ffe2
    Author: Sundeep Paruvu <sparuvu@gmail.com>
    Date:   Sat Jan 11 15:37:37 2014 -0600
    
        Added some perf. improvements in creating the DOM
    
    commit 6f17f694210f5427b5e3007547c9802bd4b4b4b8
    Author: Sundeep Paruvu <sparuvu@gmail.com>
    Date:   Sat Jan 11 14:56:15 2014 -0600
    
        space between the empty objects
    
    commit 32b141ef2420538aed0e224e97d673f45ff3e1f2
    Author: Sundeep Paruvu <sparuvu@gmail.com>
    Date:   Sat Jan 11 14:54:25 2014 -0600
    
        Rounded corners for highlighted objects
    
    commit 496bcf6785007abd53468426655f096c3c73819d
    Author: Sundeep Paruvu <sparuvu@gmail.com>
    Date:   Sat Jan 11 14:51:52 2014 -0600
    
        Fixed some perf. issues + added collapsibility for parent brace + related css
    
    commit 8f0a1f5ddd40976039bacbdd2e96babe7a44b172
    Author: Sundeep Paruvu <sparuvu@gmail.com>
    Date:   Fri Jan 10 13:10:43 2014 -0600
    
        Added the license info to the library files
    
    commit 50582c07d4f74f7d5ae1afdd2154a58a282d34d6
    Author: Sundeep Paruvu <sparuvu@gmail.com>
    Date:   Fri Jan 10 13:05:08 2014 -0600
    
        some performance improvement + code refactoring
    
    commit 5a9dcb0fe733732ea4c9d7116b6b72e7d15aaf72
    Author: Sundeep Paruvu <sparuvu@gmail.com>
    Date:   Thu Jan 9 23:00:23 2014 -0600
    
        Updated the read me for collapse option
    
    commit 14c9d8a31e76bfb19dd32ea02360e15e158777df
    Author: Sundeep Paruvu <sparuvu@gmail.com>
    Date:   Thu Jan 9 22:57:06 2014 -0600
    
        Added the Collapse option
    
    commit 40e762b7d93eb8501dc4571f31a4131c9470f03b
    Author: Sundeep Paruvu <sparuvu@gmail.com>
    Date:   Thu Jan 9 14:40:26 2014 -0600
    
        fixed the issues with the highlighting of the hovered object
    
    commit d1ffe8a98f051836c5f15d44b90fdf7e14a0796d
    Author: Sundeep Paruvu <sparuvu@gmail.com>
    Date:   Thu Jan 9 12:54:47 2014 -0600
    
        fixed the undefined error on sourceJson

--- README.md
+++ README.md
@@ -10,6 +10,11 @@ A jQuery plugin to render the formatted and collapsible
 	* Replaces the contents of `target` with formatted json		  
 * $(target).jsonFrill(sourceJson);
 	* Formats the `sourceJson` and replaces the content of the `target`		
+* $(target).jsonFrill(sourceJson, options);
+	* Formats the `sourceJson` and replaces the content of the `target`
+	* Consumes the external options.
+	* ######Options Defaults
+		* Collapse	: false
 
 ##Dependencies
 Depends on jQuery >= 1.10.2
--- src/jsonFrill.js
+++ src/jsonFrill.js
@@ -1,120 +1,148 @@
+/*!
+ * jQuery jsonFrill plugin v0.1
+ * https://github.com/sparuvu/jsonFrill
+ * 
+ * Released under the MIT license
+ * https://raw.github.com/sparuvu/jsonFrill/master/LICENSE
+ *
+ * Date: 2014-1-9
+ */
+
 ;(function ( $, window, document, undefined ) {
-	$.fn.jsonFrill = function(jsonSource, options) {
-		var jf = {}, 
-			_indentationLevel = 1, 
-			lineBreak = "</br>", 
-			seperator = " : ",
-			objOpenBrace = $('<span />').addClass('jf-obj-open-brace').text('{')[0].outerHTML
-			objCloseBrace = $('<span />').addClass('jf-obj-close-brace').text('}')[0].outerHTML
-			arrOpenBrace = $('<span />').addClass('jf-arr-close-brace').text('[')[0].outerHTML
-			arrCloseBrace = $('<span />').addClass('jf-arr-close-brace').text(']')[0].outerHTML
-			$pre = $('<pre />').css('margin', 0),
-			$ellipses = $('<span />').addClass('jf-ellipses jf-hide').text('...');
-			
-		jf.settings = $.extend({
-			'TAB': '..'
+    $.fn.jsonFrill = function(options, jsonSource) {
+        var jf = {}, 
+            _indentationLevel = 1, 
+            lineBreak = "</br>", 
+            seperator = $(document.createElement('span')).text(" : ")[0].outerHTML,
+            braces = {
+              "object": {                  
+                  open: $(document.createElement('span')).addClass('jf-brace').text('{')[0].outerHTML,
+                  close: $(document.createElement('span')).addClass('jf-brace').text('}')[0].outerHTML
+              },  
+              "array": {
+                  open: $(document.createElement('span')).addClass('jf-brace').text('[')[0].outerHTML,
+                  close: $(document.createElement('span')).addClass('jf-brace').text(']')[0].outerHTML
+              }  
+            },
+            $pre = $(document.createElement('pre')).css('margin', 0),
+            $ellipses = $(document.createElement('span')).addClass('jf-ellipses jf-hide').text('...');
+            
+        jf.settings = $.extend({
+            'collapse': false
         }, options);
         
-		function processPrimitive(key, value, type) {
-			var value = $('<span />').addClass('jf-value  jf-' + type).text(seperator + value)[0].outerHTML; 
-			return $pre.clone().html($('<div />').addClass('jf-prop').html(getKey(key) + value)[0].outerHTML)[0].outerHTML;
-		}
-		
-		function addSpaces() {
-			var emptyString = "";
-			for(var i = 0; i< _indentationLevel; i++) {
-				if(i % 2 == 0) {
-					emptyString += "  ";
-					continue;					
-				}
-				emptyString += "| ";
-			}
-			return $('<span />').addClass('jf-indents').text(emptyString)[0].outerHTML;
-		}
-		
-		function getKey(key, jfClass) {
-			jfClass = jfClass || "";
-			if(jfClass) {
-			    var key = $('<span />').addClass('jf-key').html(key)[0].outerHTML
-			    return $('<span />').addClass(jfClass).html(addSpaces() + key)[0].outerHTML
-			}
-			return $('<span />').addClass('jf-key ' + jfClass).html(addSpaces() + key)[0].outerHTML	
-		}
-		
-		function processNonPrimitive(openBrace, closeBrace, key, value) {
-		    var temp = getKey(key, "jf-collapsible-title") 
-            _indentationLevel++;                        
-            temp += seperator + openBrace + $ellipses.clone()[0].outerHTML + lineBreak + process(value, true);
+        function processPrimitive(key, value, type) {
+            var valueHtml = $(document.createElement('span')).addClass('jf-value  jf-' + type).text(value)[0].outerHTML; 
+            return $pre.clone().html($(document.createElement('div')).addClass('jf-prop').html(getKey(key) + seperator + valueHtml)[0].outerHTML)[0].outerHTML;
+        }
+        
+        function addSpaces() {
+            var emptyString = "";
+            for(var i = 0; i< _indentationLevel; i++) {
+                emptyString += "| " ;
+            }
+            return $(document.createElement('span')).addClass('jf-indents').text(emptyString)[0].outerHTML;
+        }
+        
+        function getKey(key, jfClass) {
+            jfClass = jfClass || "";
+            if(jfClass) {
+                var key = $(document.createElement('span')).addClass('jf-key jf-collapse').html(key)[0].outerHTML;
+                return $(document.createElement('span')).addClass(jfClass).html(addSpaces() + key)[0].outerHTML;
+            }
+            return $(document.createElement('span')).addClass('jf-key ' + jfClass).html(addSpaces() + key)[0].outerHTML;    
+        }
+        
+        function processNonPrimitive(openBrace, closeBrace, key, value) {
+            var temp = "";
+            _indentationLevel++;
+            var str = process(value, true);
             _indentationLevel--;
-            temp +=  addSpaces() + closeBrace;
-            return $pre.clone().addClass('jf-collapsible').html(temp)[0].outerHTML;
-		}
-		
-		function process(obj, flag) {
-			var str = "";
-			$.each(obj, function(key, value) {				
-				switch($.type(value)) {
-					case "object":
-						str += processNonPrimitive(objOpenBrace, objCloseBrace, key, value);
-						break;					
-					case "array":
-						str += processNonPrimitive(arrOpenBrace, arrCloseBrace, key, value);
-						break;
-					case "date":						
-						str += processPrimitive(key, value, "date"); 
-						flag = false;
-						break;
-					case "boolean": 
-						str += processPrimitive(key, value, "boolean");
-						flag = false;
-						break;
-					case "number": 
-						str += processPrimitive(key, value, "number");
-						flag = false;
-						break;
-					case "string": 
-						str += processPrimitive(key, value, "string");
-						flag = false;
-						break;
-					default: 
-						break;		
-				}		
-			});			
-			return str;
-		}
-		
-		function bindings() {
-		    $('.jf-collapsible-title').on('click', function(e){
-                e.preventDefault();
-                $(this).closest('pre.jf-collapsible').children('pre').slideToggle();
-                $(this).siblings('.jf-ellipses').fadeToggle('jf-hide');
-            });
+            if(str) {
+                temp = getKey(key, "jf-collapsible-title") + seperator + openBrace + $ellipses.clone()[0].outerHTML + lineBreak + str + addSpaces() + closeBrace;
+            } else {
+                temp = getKey(key) + seperator + openBrace + " " + closeBrace;
+            }            
+            return $pre.clone().addClass('jf-collapsible').html(temp)[0].outerHTML;                
+        }
                 
-            $('#jf-formattedJSON').find("*").hover(function(e){
-                $('pre.jf-highlight').removeClass('jf-highlight');
+        function process(obj, flag) {
+            var str = "";            
+            if($.isEmptyObject(obj)) {
+                return false;
+            }
+            for (var key in obj) {
+                var type = $.type(obj[key]); 
+                if(type == "object" || type == "array") {
+                    str += processNonPrimitive(braces[type].open, braces[type].close, key, obj[key]);
+                } else {
+                    str += processPrimitive(key, obj[key], type);
+                    flag = false;
+                }
+            }
+            return str;
+        }
+        
+        function toggleObjects($obj, collapse) {
+            $obj.each(function() {
+                var $this = $(this);
+                 if(collapse) {
+                    $this.closest('pre.jf-collapsible').children('pre').toggle();
+                } else {
+                    $this.closest('pre.jf-collapsible').children('pre').slideToggle();
+                }
+                $this.siblings('.jf-ellipses').fadeToggle();                    
+                $this.children('.jf-key').toggleClass('jf-collapse');
+            });
+        }
+        
+        function bindings() {
+            $("#jf-formattedJSON").on('click', '.jf-collapsible-title, .jf-parent-brace', function(e){
+                e.preventDefault();
+                toggleObjects($(this));
+            });
+            
+            $('div.jf-prop').hover(function(e) {
                   $(this).closest('pre.jf-collapsible').addClass('jf-highlight');
                   e.preventDefault();
-                }, function(e){
+                },
+                function(e) {
                   $(this).closest('pre.jf-collapsible').removeClass('jf-highlight');
                   e.preventDefault();
-            }); 
-		}
-		
-		return this.each(function() {
-			try {
-				var json = $.parseJSON(jsonSource) || $.parseJSON($(this).text());
-			}catch(ex) {
-				if(console && console.log) {
-					console.log("Invalid Json "+ex);
-				}
-				return;
-			}
-			var str = objOpenBrace + process(json, false);
-			_indentationLevel--;
-			str += addSpaces() + objCloseBrace;
-			$(this).html($pre.clone().addClass('jf-collapsible').attr('id', 'jf-formattedJSON').html(str));
-			
-			bindings();
-		});
-	}
-})(jQuery, window, document);	
+            });
+        }
+        
+        return this.each(function() {
+            try {
+                if(jsonSource) {                    
+                    if($.type(jsonSource) == "object" || $.type(jsonSource) == "array") {
+                        json = jsonSource;
+                    } else {
+                        jsonSource = jsonSource.trim();
+                        json = jsonSource.length > 0 ? $.parseJSON(jsonSource) : {} ;
+                    }
+                } else {
+                    json = $(this).text().trim();
+                    json = json.length > 0 ? $.parseJSON(json) : {} ;
+                }
+            } catch(ex) {
+                if(console && console.log) {
+                    console.log("Invalid Json "+ex);
+                }
+                return;
+            }
+            var str = process(json, false), type = $.type(json);
+            if(str) {
+                _indentationLevel--;
+                str = $(braces[type].open).addClass('jf-parent-brace')[0].outerHTML + $ellipses.clone()[0].outerHTML + str + addSpaces() + braces[type].close;
+                $(this).html($pre.clone().addClass('jf-collapsible').attr('id', 'jf-formattedJSON').html(str));                
+            } else {
+                $(this).html($pre.clone().html(braces[type].open + braces[type].close));                                    
+            }            
+            bindings();
+            if(jf.settings.collapse) {
+                toggleObjects($('#jf-formattedJSON .jf-collapsible-title'), true);
+            }
+        });
+    };
+})(jQuery, window, document);   
